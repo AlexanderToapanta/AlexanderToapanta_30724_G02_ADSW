e99358164dff12ee8285fca0bdaf906494479c21
Kenned Jhair Sigcha Palma <kjsigcha@espe.edu.ec>
2026-07-07 22:36:55 -0500

    chore(tests): configura PHPUnit y bootstrap de pruebas unitarias

---
+++ 3.Codigo/IRONBOX V1.0.0/tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../models/Usuario.php';
+require_once __DIR__ . '/../models/Membresia.php';
+require_once __DIR__ . '/../models/Clase.php';
+require_once __DIR__ . '/../models/Reserva.php';
+
+require_once __DIR__ . '/../builders/UsuarioBuilder.php';
+require_once __DIR__ . '/../builders/MembresiaBuilder.php';
+require_once __DIR__ . '/../builders/ClaseBuilder.php';
+
+require_once __DIR__ . '/../dao/UsuarioDAO.php';
+require_once __DIR__ . '/../dao/MembresiaDAO.php';
+require_once __DIR__ . '/../dao/ClaseDAO.php';
+
+require_once __DIR__ . '/../services/UsuarioService.php';
+require_once __DIR__ . '/../services/MembresiaService.php';
+require_once __DIR__ . '/../services/ClaseService.php';
