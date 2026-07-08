ecc877551578401d1a7bbdaa086040a716af4dfd
Kenned Jhair Sigcha Palma <kjsigcha@espe.edu.ec>
2026-07-07 22:38:33 -0500

    test(usuarios): UsuarioBuilder rechaza correo invalido

--- 3.Codigo/IRONBOX V1.0.0/tests/builders/UsuarioBuilderTest.php
+++ 3.Codigo/IRONBOX V1.0.0/tests/builders/UsuarioBuilderTest.php
@@ -38,4 +38,10 @@ final class UsuarioBuilderTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         (new UsuarioBuilder())->configurarCedula('1234567890');
     }
+
+    public function test_rechaza_correo_invalido(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        (new UsuarioBuilder())->configurarCorreo('no-es-un-correo');
+    }
 }
